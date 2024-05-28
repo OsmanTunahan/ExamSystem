edc2b6acdd129df5045526ebfce97a1f89edde4f
iawoken <osmantunahan07.07@gmail.com>
2024-05-28 16:26:50 +0300

    feat: add database connection

---
+++ src/main/java/org/awoken/examsystem/util/DBConnection.java
@@ -0,0 +1,21 @@
+package org.awoken.examsystem.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/exam";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Database connection failed!");
+        }
+    }
+}
