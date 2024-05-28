1397626993084d4b2eb8bfc0d694391a05a4cfdd
iawoken <osmantunahan07.07@gmail.com>
2024-05-28 16:28:26 +0300

    fix: database connection vulnerability #1

--- src/main/java/org/awoken/examsystem/util/DBConnection.java
+++ src/main/java/org/awoken/examsystem/util/DBConnection.java
@@ -3,6 +3,8 @@ package org.awoken.examsystem.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/exam";
@@ -14,8 +16,9 @@ public class DBConnection {
             Class.forName("com.mysql.cj.jdbc.Driver");
             return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Database connection failed!");
+            Logger logger = Logger.getLogger(DBConnection.class.getName());
+            logger.log(Level.SEVERE, "Database connection failed!", e);
+            throw new RuntimeException("Database connection failed!", e);
         }
     }
 }
